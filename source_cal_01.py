ecc9eb1aa652a5090520859c5fb52fcc966ec1bc
kzt206 <kzt206@gmail.com>
2021-10-12 10:02:03 +0900

    cal transpose G

--- source_cal_01.py
+++ source_cal_01.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 # 計算回数と最大値の設定
-numCal = 1
-maxCal = 10
+numCal = 0
+maxCal = 1
+
+# 計算グリッドの設定
+grid = np.array([0.1,0.1,0.,0.01])
+dx = 0.1
+dy = 0.1
+dz = 0.
+dt = 0.01
 
 # 地震計の位置と観測された地震波の到達時間の設定
 numPoints = 10
@@ -14,7 +21,7 @@ ts = np.array([10.477,9.759,9.243,4.984,7.499,2.980,4.409,5.817,10.184,9.274])
 
 di = np.zeros(numPoints)
 dd = np.zeros(numPoints)
-g = np.zeros((numPoints,4))
+
 # print(g)
 
 # 震源の位置と時刻の初期値を設定
@@ -23,8 +30,10 @@ mi = np.array([-10,10,20,0]) # set a temporal initial source
 #地震波速度
 V = 5 #km/s
 
+# 更新回数が設定回数を上回った？
 while numCal < maxCal:
     print("\nnumCal: " + str(numCal) )
+    G = np.zeros((numPoints,4))
     # 予測される地震波の到達時刻の計算
     di = np.sqrt(np.square(xs-mi[0]) + np.square(ys-mi[1]) + np.square(zs-mi[2])) /V + mi[3]
     print(di)
@@ -32,5 +41,42 @@ while numCal < maxCal:
     # 観測との差を計算
     dd = ts - di 
     print(dd)
+
+    # 行列Gを求める G(i,j) = d(di)/d(mj)
+    # mi_0 = mi + np.array([dx,0,0,0])
+    # di_0 = np.sqrt(np.square(xs-mi_0[0]) + np.square(ys-mi_0[1]) + np.square(zs-mi_0[2])) /V + mi_0[3]
+    # G[:,0] = (di_0 - di) / dx
+    # print("G:")
+    # print(G[:,0])
+
+    print("j:")
+    for j in range(4):
+        print(j)
+        grid_tmp = np.zeros(4)
+        grid_tmp[j] = grid[j]
+        print(grid_tmp)
+        mi_tmp = mi + grid_tmp
+        di_tmp = np.sqrt(np.square(xs-mi_tmp[0]) + np.square(ys-mi_tmp[1]) + np.square(zs-mi_tmp[2])) /V + mi_tmp[3]
+        if grid[j] != 0:
+            G[:,j] = (di_tmp - di) / grid[j]
+        else:
+            G[:,j] = 0
+    print("G:")
+    print(G)
+
+    # 正方行列　GtG を求める
+    Gt = G.transpose()
+    print("Gt:")
+    print(Gt)
+
+    # (GtG)-1　を求める
+
+    # dm = (GtG)-1 Gt Ddを求める
+
+
+    # dmを使って震源の位置と時刻の値を更新する
+
+
+    # dmの絶対値はゼロに近い？　ー＞　おわり
 
     numCal += 1
